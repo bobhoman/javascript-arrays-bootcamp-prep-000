4663a3bfdb0ee20b7edf89195c446cd3d8347681
bobhoman <bobhoman25@gmail.com>
2017-12-14 06:50:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift(0)
+  return array.shift(1)
 }
 
 function removeElementFromBeginningOfArray(array) {
